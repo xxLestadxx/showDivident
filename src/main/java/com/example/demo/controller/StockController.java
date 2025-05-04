de37b8a74aad813f6634a32cdba687707fe4ad51
Yordan <j.v.gospodinov@gmail.com>
2025-05-04 19:37:00 +0300

    updt

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -1,4 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.StockService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stocks")
 public class StockController {
+
+    private final StockService stockService;
+
+    public StockController(StockService stockService){
+        this.stockService = stockService;
+    }
+
+    @GetMapping("/{symbol}")
+    public ResponseEntity<String> getStockData(@PathVariable String symbol){
+        String res = stockService.getDailyStockData(symbol);
+        return ResponseEntity.ok(res);
+    }
 }
--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -1,7 +1,27 @@
-package com.example.demo;
+package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StockService {
+    @Value("${alpha.vantage.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+    public StockService (RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
+    }
+
+    public String getDailyStockData(String symbol){
+        String url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY" +
+                "&symbol=" + symbol +
+                "&apikey=" + apiKey;
+
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
 }
